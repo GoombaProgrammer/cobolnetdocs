c0e6dcf0c37c2b6aeb9c01a3d4716b76987c7b27
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-03-26 08:21:46 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
+project = 'Super COBOL.NET'
+copyright = '2022, GoombaProgrammer/Ava#7777'
 author = 'Graziella'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
